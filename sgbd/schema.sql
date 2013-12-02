6dd01b81306ee46c256a988172d25cbce9c8cd6e
Colin Vidal <colin@cvidal.org>
2013-12-02 17:09:20 +0100

    navigations dans le schéma ok contrainte unique libelle ok dans les deux tables graces aux triggers pl/sql

--- sgbd/schema.sql
+++ sgbd/schema.sql
@@ -30,18 +30,50 @@ CREATE OR REPLACE TYPE DVEDETTE_REF_T AS OBJECT
 (REF_DESC REF DVEDETTE_T)
 /
 
---drop type descripteur_t force;
---drop type dnormal_t force;
---drop type dnormal_ref_t force;
---drop type dvedette_ref_t force;
---drop type dnormal_ref_nested_t force;
---drop type dvedette_ref_nested_t force;
---drop type dvedette_t force;
 
--- CRÉATION DE MÉTHODES
+
+
+-- CRÉATION DE TRIGGERS
+
+CREATE OR REPLACE TRIGGER CHK_CONSTRAINT_UNIQUE_DNORMAL
+BEFORE UPDATE OR INSERT ON DNORMAL
+FOR EACH ROW
+DECLARE
+ result number;
+BEGIN
+	select count(*) into result from dvedette where libelle=:new.libelle;
+  if result > 0 then
+    raise_application_error(-20001,'Libellé déjà utilisé dans DVEDETTE');
+  end if;
+ 	select count(*) into result from dnormal where libelle=:new.libelle;
+  if result > 0 then
+    raise_application_error(-20001,'Libellé déjà utilisé dans DNORMAL');
+  end if;
+END;
+/
+
+CREATE OR REPLACE TRIGGER CHK_CONSTRAINT_UNIQUE_DVEDETTE
+BEFORE UPDATE OR INSERT ON DVEDETTE
+FOR EACH ROW
+DECLARE
+ result number;
+BEGIN
+	select count(*) into result from dvedette where libelle=:new.libelle;
+  if result > 0 then
+    raise_application_error(-20001,'Libellé déjà utilisé dans DVEDETTE');
+  end if;
+ 	select count(*) into result from dnormal where libelle=:new.libelle;
+  if result > 0 then
+    raise_application_error(-20001,'Libellé déjà utilisé dans DNORMAL');
+  end if;
+END;
+/
+
+
 
 
 -- CRÉATION DES TABLES
+
 CREATE TABLE DNORMAL OF DNORMAL_T
 (PRIMARY KEY (LIBELLE),
 CONSTRAINT DNORMAL_NOTNULL_REF_VEDETTE REF_VEDETTE NOT NULL);
@@ -51,3 +83,16 @@ CREATE TABLE DVEDETTE OF DVEDETTE_T
 (PRIMARY KEY (LIBELLE))
 NESTED TABLE SYNONYMES STORE AS DVEDETTE_SYNONYMES
 NESTED TABLE FILS STORE AS DVEDETTE_FILS;
+
+
+
+
+-- DEBUG
+
+--drop type descripteur_t force;
+--drop type dnormal_t force;
+--drop type dnormal_ref_t force;
+--drop type dvedette_ref_t force;
+--drop type dnormal_ref_nested_t force;
+--drop type dvedette_ref_nested_t force;
+--drop type dvedette_t force;
--- sgbd/tests.sql
+++ sgbd/tests.sql
@@ -1,10 +1,34 @@
--- ajout racine
-insert into dvedette values (dvedette_t('Animal', NULL, dvedette_ref_nested_t(), NULL));
+-- pour les tests, j'utilise des majuscule, mais tout les libelles doivent être minuscule dans la bdd
 
--- ajout noeud
-insert into dvedette values ('Vertébré', (select ref(d) from dvedette d where d.libelle='Animal'), dvedette_ref_nested_t(), NULL);
-insert into table (select d.fils from dvedette d where d.libelle='Animal') values (dvedette_ref_t (select ref(d2) from dvedette d2 where d2.libelle='Vertébré'));
+-- ajout racine animal
+insert into dvedette values (dvedette_t('Animal', NULL, dvedette_ref_nested_t(), dnormal_ref_nested_t()));
 
 
-insert into DVedette values ('Félin', (select ref(d) from DVedette d where d.libelle='Vertébré'), NULL);
-insert into DVedette values ('Chat', (select ref(d) from DVedette d where d.libelle='Félin'), NULL);
+-- ajout noeud en dessous de animal
+insert into dvedette values ('Vertébré', (select ref(d) from dvedette d where d.libelle='Animal'), dvedette_ref_nested_t(), dnormal_ref_nested_t());
+insert into table (select d.fils from dvedette d where d.libelle='Animal') values (dvedette_ref_t ((select ref(d2) from dvedette d2 where d2.libelle='Vertébré')));
+
+insert into dvedette values ('Amphibia', (select ref(d) from dvedette d where d.libelle='Animal'), dvedette_ref_nested_t(), dnormal_ref_nested_t());
+insert into table (select d.fils from dvedette d where d.libelle='Animal') values (dvedette_ref_t ((select ref(d2) from dvedette d2 where d2.libelle='Amphibia')));
+
+
+-- ajout de félin et de chat sous vertébré
+insert into dvedette values ('Félin', (select ref(d) from dvedette d where d.libelle='Vertébré'), dvedette_ref_nested_t(), dnormal_ref_nested_t());
+insert into table (select d.fils from dvedette d where d.libelle='Vertébré') values (dvedette_ref_t ((select ref(d2) from dvedette d2 where d2.libelle='Félin')));
+
+insert into dvedette values ('Chat', (select ref(d) from dvedette d where d.libelle='Félin'), dvedette_ref_nested_t(), dnormal_ref_nested_t());
+insert into table (select d.fils from dvedette d where d.libelle='Félin') values (dvedette_ref_t ((select ref(d2) from dvedette d2 where d2.libelle='Chat')));
+
+
+-- ajout de synonyme à chat
+insert into dnormal values (dnormal_t('minou', (select ref(d) from dvedette d where d.libelle='Chat')));
+insert into table (select d.synonymes from dvedette d where d.libelle='Chat') values (dnormal_ref_t ((select ref(d2) from dnormal d2 where d2.libelle='minou')));
+
+
+
+-- sélections des fils de Animal (ou n'importe quel object)
+select deref(t.ref_desc).libelle from table (select fils from dvedette where libelle='Animal') t;
+
+-- sélection des synonymes du descripteur vedette 'chat'
+select deref(t.ref_desc).libelle from table (select synonymes from dvedette d where libelle='Chat') t;
+
