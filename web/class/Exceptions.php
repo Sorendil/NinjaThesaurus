528dd6e95bb70e753e97393e29c0bb16fb8fbc19
Colin Vidal <colin@cvidal.org>
2013-12-09 16:40:16 +0100

    --

--- web/class/Exceptions.php
+++ web/class/Exceptions.php
@@ -2,12 +2,15 @@
 
 class NotFoundException extends Exception
 {
+  echo 'Descripteur non trouv&eacute;.';
 }
 
 class AlreadyExistingException extends Exception
 {
+  echo 'Decripteur d&eacute;j&agrave; existant.';
 }
 
 class WrongArgsException extends Exception
 {
+  echo 'Param&egrave;tres incorrect.';
 }
