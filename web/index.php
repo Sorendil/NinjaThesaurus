2160358dbf743fd6dd8ec655da94b8a2c01671e8
Mailys07 <mailys.denis@aliceadsl.fr>
2013-12-23 12:39:39 +0100

    maj index.php

--- web/index.php
+++ web/index.php
@@ -101,11 +101,13 @@ if (isset ($_GET['libelle']))
 	}
 
 	echo "\n <h3>Synonymes</h3> ";
-	echo "<div id=\"form-recherche\"><form class=\"form-search\" action=\"#\" method=\"post\">
+	echo "</pre>";
+	echo "<form class=\"form-search\" action=\"#\" method=\"post\">
 				<input type=\"hidden\" name=\"rel\" value=\"\" />
 				<input type=\"text\" name=\"libelle\" class=\"input-medium search-query\">
 				<button type=\"submit\" class=\"btn\" name=\"add\">Ajout</button>
-			</form></div>";
+			</form>";
+	echo "<pre>";
 	echo "<hr/> \n";
 	for($j=0; $j<count($synonyme); $j++){
 		echo '<a href="http://cvidal.org:81/'.$relations['LIBELLE_DESC'][$synonyme[$j]].'/">'.$relations['LIBELLE_DESC'][$synonyme[$j]]."</a> ";
