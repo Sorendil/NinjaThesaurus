babeac5fdf498883b7647514b18895f1156fb82c
Mailys07 <mailys.denis@aliceadsl.fr>
2013-12-24 13:35:21 +0100

    maj index

--- web/index.php
+++ web/index.php
@@ -89,9 +89,9 @@
 					for($i=0; $i<count($relations['LIBELLE_REL']); $i++){
 						echo "2";
 						echo "taille".count($tab_rel);
-						for($j=0; $j<(count($tab_rel)+1); $j++){
-						echo "3";
-							if((count($tab_rel)==0) ||($relations['LIBELLE_REL'][$i]!=$tab_rel[$j])){
+						foreach($tab_rel as $eky => $value){
+							echo "3";
+							if((count($tab_rel)==0) ||($relations['LIBELLE_REL'][$i]!=$key)){
 								echo "4";
 								$tab_rel[$relations['LIBELLE_REL'][$i]]=array();
 								array_push($tab_rel[$relations['LIBELLE_REL'][$i]], $i);
