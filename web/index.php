2b18e4af05c05ee2b6cbd941fe528eadfa7a50ec
Mailys07 <mailys.denis@aliceadsl.fr>
2013-12-24 11:58:26 +0100

    maj index.php

--- web/index.php
+++ web/index.php
@@ -101,6 +101,7 @@
 						}								
 								
 					}
+					echo "\n".print_r($tab_rel);
 					echo "5";
 					for($i=0; $i<count($tab_rel); $i++){
 						echo "<h3>".$tab_rel[$i]."</h3>";
@@ -108,7 +109,7 @@
 							echo '<a href="http://cvidal.org:81/'.$relations['LIBELLE_DESC'][$tab_rel[$i][$j]].'/">'.$relations['LIBELLE_DESC'][$tab_rel[$i][$j]].'</a> ';//attention affiche juste l'indice pas les mots de libelle_desc
 						}
 						echo '<form class=\"form-search\" action=\"\" method=\"post\">
-								<input type=\"hidden\" name=\"rel\" value='.$tab_rel[$i].' />
+								<input type=hidden name=rel value='.$tab_rel[$i].' />
 								<input type=\"text\" name=\"libelle\" class=\"input-medium search-query\">
 								<button type=\"submit\" class=\"btn\" name=\"add\">Ajout</button>
 							</form>';
