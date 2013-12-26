f328f60fab18c7b6f0efddd3e257214830104221
Mailys07 <mailys.denis@aliceadsl.fr>
2013-12-26 22:01:49 +0100

    maj index.php

--- web/index.php
+++ web/index.php
@@ -97,7 +97,7 @@
 							
 						}								
 						
-						array_push($tab_rel[$relations['LIBELLE_REL'][$i]], $i);		
+						$tab_rel[$relations['LIBELLE_REL'][$i]]= $i;		
 					}
 					
 					print_r($tab_rel);
